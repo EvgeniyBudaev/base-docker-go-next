d8a01ecb1b3476dee0b38fdcd998fc5969bdfd97
Evgeniy Budaev <budaev.e@gmail.com>
2024-04-25 20:48:51 +0300

    feat: web env 3

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone",
+    // output: "standalone",
     experimental: {
         serverActions: {
             allowedOrigins: ["backend:8000", "localhost:3000"]
