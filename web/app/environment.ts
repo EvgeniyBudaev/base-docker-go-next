5dbfbe51315da33455c6f25e19d2d3c453210578
Evgeniy Budaev <budaev.e@gmail.com>
2024-04-25 19:14:40 +0300

    feat: web env test2

--- web/app/environment.ts
+++ web/app/environment.ts
@@ -1,6 +1,6 @@
 export type EnvironmentType = {
-  NEXT_PUBLIC_API_URL: string;
-  NEXT_PUBLIC_NODE_ENV: string;
+  NEXT_PUBLIC_API_URL: string | undefined;
+  NEXT_PUBLIC_NODE_ENV: string | undefined;
 };
 
 const { NEXT_PUBLIC_API_URL, NEXT_PUBLIC_NODE_ENV } = process?.env;
@@ -9,6 +9,6 @@ const { NEXT_PUBLIC_API_URL, NEXT_PUBLIC_NODE_ENV } = process?.env;
  * Переменные окружения
  */
 export const Environment: EnvironmentType = {
-  NEXT_PUBLIC_API_URL: NEXT_PUBLIC_API_URL ?? undefined,
-  NEXT_PUBLIC_NODE_ENV: NEXT_PUBLIC_NODE_ENV ?? undefined,
+  NEXT_PUBLIC_API_URL: NEXT_PUBLIC_API_URL ?? "undefined",
+  NEXT_PUBLIC_NODE_ENV: NEXT_PUBLIC_NODE_ENV ?? "undefined",
 };
