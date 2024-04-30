944104428acd5dd4b0e94c1b5df14da6913d3ac1
Evgeniy Budaev <budaev.e@gmail.com>
2024-04-30 15:17:08 +0300

    feat: optimization web Dockerfile

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: "standalone",
-    experimental: {
-        serverActions: {
-            allowedOrigins: ["backend:8000", "localhost:3000"]
-        }
-    }
+    output: "standalone",
 };
 
 export default nextConfig;
